182c0db53d3444c7f2b36a147abe6003456d8f7a
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-06-05 18:08:35 -0700

    [B] Project Startup
    - The application runs on a separate folder: menstrual-cycle-tracker
    - Necessary libraries (React, TypeScript, Capacitor , localForage and extra libraries that are needed to compile the app like React Route and it's respective types are also installed)
    - Squash (compiling) bugs - the project is basically buildable (but should not)

---
+++ menstrual-cycle-tracker/capacitor.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  appId: 'com.example.menstrualcycletracker',
+  appName: 'Menstrual Cycle Tracker',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+    StatusBar: {
+      style: 'DARK',
+    },
+  },
+};
