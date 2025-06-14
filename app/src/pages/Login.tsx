58d318097d7c645aeb193fc1a7a0d91d9b5c95a2
Minami Sato <minamisato@Minamis-MacBook-Pro.local>
2025-06-13 23:16:25 -0700

    [M] add Tailwind, update React

---
+++ app/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Home: React.FC = () => {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default Home;
