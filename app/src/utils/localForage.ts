6a5f7050e43a698c5c5d0393fb3fd1e2ca9eb17f
Estella A <e.stellaa1132@gmail.com>
2025-06-17 19:12:52 -0700

    added basic validation for schema to helpers, chaged localforage version to v10

--- app/src/utils/localForage.ts
+++ app/src/utils/localForage.ts
@@ -1,6 +1,13 @@
 import localForage from 'localforage';
 
-const storeCycleData = async (key: string, data: any) => {
+//note: validation will need to be changed if localforage is used to store other data (like settings)
+
+const storeCycleData = async (data: any) => {
+    if(!isValidEntry(data)){
+        console.error("Invalid cycle data");
+        return;
+    }
+        const key = data.MenstruationStarts;
     try {
         await localForage.setItem(key, data);
     } catch (error) {
@@ -25,5 +32,20 @@ const removeCycleData = async (key: string) => {
         console.error('Error removing cycle data:', error);
     }
 };
+//basic validation, does not validate user input
+function isValidEntry(data: any) {
+    return (
+        data &&
+        "MenstruationStarts" in data &&
+        "MenstruationEnds" in data &&
+        "OvulationStarts" in data &&
+        "OvulationEnds" in data &&
+        "UserNote" in data &&
+
+        "fertileWindowStart" in data &&
+        "fertileWindowEnd" in data &&
+        "averageCycleLength" in data
+    );
+}
 
 export { storeCycleData, getCycleData, removeCycleData };
