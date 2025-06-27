dc5918d8ad344cce152c97924189b52e720e73a0
Minami Sato <minamisato@Minamis-MacBook-Pro.local>
2025-06-26 22:29:40 -0700

    [M]added a calendar view

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -3,7 +3,8 @@ import { MenstruationCalendar } from "@/features/MenstrumentCalendar"
 export default function Home() {
   return (
     <div>
-      <MenstruationCalendar />
+        <h1>Open Cycle Calendar</h1>
+            <MenstruationCalendar />
     </div>
   )
 }
