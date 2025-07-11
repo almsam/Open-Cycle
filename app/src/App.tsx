d1ac82f54ea882b219a8789cae591d1ebca48cae
System Administrator <root@Minamis-MacBook-Pro.local>
2025-07-11 16:26:02 -0700

    addd login page and setup sidebar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Outlet } from 'react-router-dom'
 import Home from './pages/Home'
 import Settings from './pages/Settings'
+import LoginPage from './pages/Login'
 import Sidebar from "@/components/ui/sidebar"
+
 import { Toaster } from 'sonner'
+
+function AppLayout() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="flex">
-      <Sidebar/>
-
-      <Routes>
-        <Route path="/home" element={<Home />}/>
-        <Route path="/settings" element={<Settings />}/>
-        
-      </Routes>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="home" element={<Home />}/>
+            <Route path="settings" element={<Settings />}/>
+          </Route>
+          <Route path="/login" element={<LoginPage />}/>
+        </Routes>
       <Toaster />
     </div>
   )
