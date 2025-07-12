df2df99177f53c363dc45c5a67d098d5282dfab4
Alexofthefoot <73742087+Alexofthefoot@users.noreply.github.com>
2025-07-12 00:23:20 -0700

    [A] localforage & local data typing

---
+++ app/src/localforage/index.js
@@ -0,0 +1,5 @@
+// Collects and re-exports from the multiple files
+import { setUserInStorage, getCycleById } from './localforage';
+
+export * from './userStorage.js';
+export * from './cycleStorage.js';
