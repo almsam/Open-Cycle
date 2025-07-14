282efb0abc8495de91fd147d964c3054abf3e90f
Estella A <e.stellaa1132@gmail.com>
2025-07-13 23:09:36 -0700

    [E] added note.tsx, minor sidebar change

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Settings from './pages/Settings'
+import Note from './pages/Note'
 import Sidebar from "@/components/ui/sidebar"
 import { Toaster } from 'sonner'
 
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />}/>
         <Route path="/settings" element={<Settings />}/>
-        
+        <Route path="/note" element ={<Note />}/>
       </Routes>
       <Toaster />
     </div>
