a90554f9c722b5a5e47bb98dfa4e00ceb007f07e
System Administrator <root@Minamis-MacBook-Pro.local>
2025-07-20 23:05:09 -0700

    [M] added yearly-calendar view

--- app/src/features/MenstrumentCalendar.tsx
+++ app/src/features/MenstrumentCalendar.tsx
@@ -4,7 +4,8 @@ import { Calendar } from "@/components/ui/calendar"
 import { Card } from "@/components/ui/card"
 import { eachDayOfInterval, addDays, isSameDay } from "date-fns"
 import { useState } from "react"
-
+//import  Calendar from 'react-yearly-calendar';
+import './calstyles.css';
 
 
 export function MenstruationCalendar() {
@@ -14,14 +15,17 @@ export function MenstruationCalendar() {
     end: addDays(new Date(), 5),
   }
   )
+  const year = new Date().getFullYear();
+  //const Calendar = require('react-yearly-calendar');
 
   return (
     
-    <Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262]">
+    //<Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262]">
+      <div className="grid grid-cols-4">
         <Calendar
         mode="single"
         defaultMonth={new Date()}
-        numberOfMonths={1}
+        numberOfMonths={12}
         selected={date}
         onSelect={setDate}
         modifiers={{
@@ -34,10 +38,12 @@ export function MenstruationCalendar() {
             max-w-[500px] mx-auto text-base
             [&_[data-selected-single='true']]:bg-[#a30262]
             [&_[data-selected-single='true']]:text-white
-            [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
-        captionLayout="dropdown"
+            [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl
+            content-start"
+        //captionLayout="dropdown"
         />
-    </Card>
+      </div>
+    //</Card>
     
   )
 }
---
+++ app/src/types/react-yearly-calendar.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-yearly-calendar'{
+    export function Calendar();
+}
