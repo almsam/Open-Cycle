9f5d7d117d44db78de5359c6889be6f82116f339
System Administrator <root@Minamis-MacBook-Pro.local>
2025-07-27 07:35:53 -0700

    [M] display the clicked date below the calendar

--- app/src/features/MenstrumentCalendar.tsx
+++ app/src/features/MenstrumentCalendar.tsx
@@ -14,29 +14,34 @@ export function MenstruationCalendar() {
     end: addDays(new Date(), 5),
   }
   )
+  
 
   return (
-    
-    <Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262]">
-        <Calendar
-        mode="single"
-        defaultMonth={new Date()}
-        numberOfMonths={1}
-        selected={date}
-        onSelect={setDate}
-        modifiers={{
-        highlight: highlightDays,
-        }}
-        modifiersClassNames={{
-        highlight: "bg-[#d162a4]",
-        }}
-        className="
-            max-w-[500px] mx-auto text-base
-            [&_[data-selected-single='true']]:bg-[#a30262]
-            [&_[data-selected-single='true']]:text-white
-            [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
-        />
-    </Card>
+    <div>
+      <Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262] mb-8">
+          <Calendar
+          mode="single"
+          defaultMonth={new Date()}
+          numberOfMonths={1}
+          selected={date}
+          onSelect={setDate}
+          modifiers={{
+          highlight: highlightDays,
+          }}
+          modifiersClassNames={{
+          highlight: "bg-[#d162a4]",
+          }}
+          className="
+              max-w-[500px] mx-auto text-base
+              [&_[data-selected-single='true']]:bg-[#a30262]
+              [&_[data-selected-single='true']]:text-white
+              [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
+          />
+      </Card>
+      <text>{
+          date ? `Selected: ${date.toLocaleDateString()}` : "Select a day to see details."
+        }</text>
+    </div>
     
   )
 }
