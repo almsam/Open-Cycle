ff62f0e6d54396c68e64f867b4bde1c04466591d
System Administrator <root@Minamis-MacBook-Pro.local>
2025-08-03 22:05:56 -0700

    [M] added a legend

--- app/src/features/MenstrumentCalendar.tsx
+++ app/src/features/MenstrumentCalendar.tsx
@@ -2,6 +2,7 @@
 import * as React from "react"
 import { Calendar } from "@/components/ui/calendar"
 import { Card } from "@/components/ui/card"
+import { Label } from "@radix-ui/react-label"
 import { eachDayOfInterval, addDays, isSameDay } from "date-fns"
 import { useState } from "react"
 import type { url } from "inspector"
@@ -27,6 +28,7 @@ export function MenstruationCalendar() {
 
   return (
     <div>
+      <div className="flex items-start ...">
       <Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262] mb-8">
           <Calendar
           mode="single"
@@ -47,6 +49,16 @@ export function MenstruationCalendar() {
               [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
           />
       </Card>
+      <Card className="ml-6 w-96 ... border-[2px] border-[#00bfff]">
+      <div id='legend' className="ml-2 place-items-start">
+        <span className='today'> Today</span>
+        <span className='previous'> Previous Cycle</span>
+        <span className='next'> Next Cycle</span>
+        <span className='note'> Day with a Note</span>
+      </div>
+      </Card>
+      </div>
+      <Card>
       <span>{
           date ? ` ${formattedDate}` : "Select a day to see details."
         }
@@ -59,6 +71,7 @@ export function MenstruationCalendar() {
       <span>Make a note </span>
       {/*TODO: Change the link to the date */}
       <a href="http://localhost:5173/date">here.</a>
+      </Card>
     </div>
     
   )
