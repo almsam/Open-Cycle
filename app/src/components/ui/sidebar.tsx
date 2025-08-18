9a786eabf858834039a661dbdd6e08c7527d571c
System Administrator <root@Minamis-MacBook-Pro.local>
2025-08-17 23:34:55 -0700

    [M] aligned icons and name of them in a column in android device

--- app/src/components/ui/sidebar.tsx
+++ app/src/components/ui/sidebar.tsx
@@ -1,6 +1,6 @@
 // components/ui/sidebar.tsx
 
-import { Home, NotebookPen, Settings } from "lucide-react"
+import { Home, NotebookPen, Settings, LogOut } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useNavigate, useLocation } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
@@ -21,21 +21,26 @@ export default function Sidebar() {
   }`}>
       <h2 className="text-xl font-bold text-[#a30262]">Open Cycle</h2>
 
-      <nav className="flex flex-col space-y-2">
-        <Button variant="ghost" className={`justify-start ${isActive("home") ? "font-bold text-[#a30262]" : ""}`} onClick={() => navigate('home')}>
+      <nav className={`space-y-2 ${
+        isAndroid
+          ? "flex"
+          : "flex flex-col"
+      }`}>
+        <Button variant="ghost" className={`justify-start ${isActive("home") ? "font-bold text-[#a30262]" : ""}${isAndroid ? "flex flex-col" : ""}`} onClick={() => navigate('home')}>
           <Home className="mr-2 h-5 w-5" />
           Home
         </Button>
-        <Button variant="ghost" className={`justify-start ${isActive("note") ? "font-bold text-[#a30262]" : ""}`} onClick={() => navigate('note')}>
+        <Button variant="ghost" className={`justify-start ${isActive("note") ? "font-bold text-[#a30262]" : ""}${isAndroid ? "flex flex-col" : ""}`} onClick={() => navigate('note')}>
           <NotebookPen className="mr-2 h-5 w-5" />
           Note
         </Button>
-        <Button variant="ghost" className={`justify-start ${isActive("setting") ? "font-bold text-[#a30262]" : ""}`} onClick={() => navigate('settings')}>
+        <Button variant="ghost" className={`justify-start ${isActive("setting") ? "font-bold text-[#a30262]" : ""}${isAndroid ? "flex flex-col" : ""}`} onClick={() => navigate('settings')}>
           <Settings className="mr-2 h-5 w-5" />
           Settings
         </Button>
         <ModeToggle />
-        <Button variant="ghost" className="justify-start" onClick={() => navigate('signout')}>
+        <Button variant="ghost" className={`justify-start ${isAndroid ? "flex flex-col" : ""}`}onClick={() => navigate('signout')}>
+          <LogOut className="mr-2 h-5 w-5" />
           Sign out
         </Button>
 
