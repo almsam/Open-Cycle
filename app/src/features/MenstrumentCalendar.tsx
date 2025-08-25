a4e858a8a64c6468edb41201dc1787928482e885
System Administrator <root@Minamis-MacBook-Pro.local>
2025-08-24 21:53:01 -0700

    [M] swapped and resized legend card and calendar card

--- app/src/features/MenstrumentCalendar.tsx
+++ app/src/features/MenstrumentCalendar.tsx
@@ -9,10 +9,12 @@ import { useState } from "react"
 //import type { url } from "inspector"
 import {NotebookPen} from "lucide-react"
 import { useNavigate } from "react-router-dom";
+import { Capacitor } from "@capacitor/core";
 
 
 
 export function MenstruationCalendar() {
+  const isAndroid = Capacitor.getPlatform() === "android";
   const navigate = useNavigate();
   //const url = "http://localhost:5173/note";
   const [date, setDate] = useState<Date | undefined>(new Date())
@@ -31,35 +33,35 @@ export function MenstruationCalendar() {
 
   return (
     <div>
-      <div className="flex items-start ...">
-      <Card className="max-w-[560px] w-full mx-auto p-6 border-[2px] border-[#a30262] mb-8">
-          <Calendar
-          mode="single"
-          defaultMonth={new Date()}
-          numberOfMonths={1}
-          selected={date}
-          onSelect={setDate}
-          modifiers={{
-          highlight: highlightDays,
-          }}
-          modifiersClassNames={{
-          highlight: "bg-[#d162a4]",
-          }}
-          className="
-              max-w-[500px] mx-auto text-base
-              [&_[data-selected-single='true']]:bg-[#a30262]
-              [&_[data-selected-single='true']]:text-white
-              [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
-          />
-      </Card>
-      <Card className="ml-6 w-96 ... border-[2px] border-[#00bfff]">
-      <div id='legend' className="ml-2 place-items-start">
-        <span className='today'> Today</span>
-        <span className='previous'> Previous Cycle</span>
-        <span className='next'> Next Cycle</span>
-        <span className='note'> Day with a Note</span>
-      </div>
-      </Card>
+      <div className={isAndroid ? "flex flex-col" : "flex items-start"}>
+        <Card className={`border-[2px] border-[#a30262] w-full ${isAndroid ? "order-last" : "max-w-[560px] mx-auto p-6  mb-8" }`}>
+            <Calendar
+            mode="single"
+            defaultMonth={new Date()}
+            numberOfMonths={1}
+            selected={date}
+            onSelect={setDate}
+            modifiers={{
+            highlight: highlightDays,
+            }}
+            modifiersClassNames={{
+            highlight: "bg-[#d162a4]",
+            }}
+            className="
+                max-w-[500px] mx-auto text-base
+                [&_[data-selected-single='true']]:bg-[#a30262]
+                [&_[data-selected-single='true']]:text-white
+                [&_button]:h-12 [&_button]:w-12 [&_button]:text-xl"
+            />
+        </Card>
+        <Card className={`border-[2px] border-[#00bfff] ${isAndroid ? "order-first" : "ml-6 w-96 ..."}`}>
+        <div id='legend' className="ml-2 place-items-start">
+          <span className='today'> Today</span>
+          <span className='previous'> Previous Cycle</span>
+          <span className='next'> Next Cycle</span>
+          <span className='note'> Day with a Note</span>
+        </div>
+        </Card>
       </div>
       <Card className="border-[2px] border-[#a30262]">
       <div>
